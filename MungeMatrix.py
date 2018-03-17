39b7ba58c304b01915c9c7424e1bf8c4e6a7eb8a
dashademidova <35659527+dashademidova@users.noreply.github.com>
2018-03-17 13:51:13 +0300

    outer & eigvals
    outer product instead of dot product
    misprint in eigenvals: eigvals

--- MungeMatrix.py
+++ MungeMatrix.py
@@ -2,10 +2,10 @@ import numpy as np
 
 def MungeMatrix(v):
 	vScale = np.sqrt(np.abs(np.diag(v)))
-	vScale = vScale.dot(vScale.T)
+	vScale = np.outer(vScale, vScale)
 	vScale[vScale == 0] = 1e-8
 	standardizedV = v / vScale
-	m = np.min(np.linalg.eigenvals(standardizedV))
+	m = np.min(np.linalg.eigvals(standardizedV))
 	if m < 0:
 		standardizedV -= 2 * m * np.eye(v.shape[0])
 		standardizedV /= 1 - 2 * m 
